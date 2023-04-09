6fbcaa78d969d8f9beb81cead55746989b66dc89
designasabaker <95067357+designasabaker@users.noreply.github.com>
2023-04-09 01:15:29 -0400

    depoly3

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
 					<AppHeader />
 					<Suspense fallback={""}>
 						<Routes>
-							<Route path="/" element={<Home />} />
+							<Route path="home" element={<Home />} />
 							<Route path="projects" element={<Projects />} />
 							<Route
 								path="projects/:projectName"
