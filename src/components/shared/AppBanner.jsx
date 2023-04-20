88b3067f737764878b85c79e3bd04e214c8e2b29
designasabaker <95067357+designasabaker@users.noreply.github.com>
2023-04-20 15:02:16 -0400

    Update homepage gif style

--- src/components/shared/AppBanner.jsx
+++ src/components/shared/AppBanner.jsx
@@ -75,11 +75,11 @@ const AppBanner = () => {
 				initial={{ opacity: 0, y: -180 }}
 				animate={{ opacity: 1, y: 0 }}
 				transition={{ ease: 'easeInOut', duration: 0.9, delay: 0.2 }}
-				className="w-full sm:w-2/3 text-right float-right mt-8 sm:mt-0"
+				className="w-full sm:w-1/3 text-right float-right mt-8 sm:mt-0"
 			>
 					<img
-						className="w-full"
-						onClick={executeScroll} className="cursor-pointer"
+						className="w-full rounded-lg cursor-pointer"
+						onClick={executeScroll}
 						src={
 							activeTheme === 'dark' ? testGIF : testGIF
 						}
