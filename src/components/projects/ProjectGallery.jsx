34dcf2599b99ad9add19b7dcc2fd790a8cf12d8d
designasabaker <95067357+designasabaker@users.noreply.github.com>
2023-06-21 15:26:15 -0400

    Implement clickable image to larger preview

--- src/components/projects/ProjectGallery.jsx
+++ src/components/projects/ProjectGallery.jsx
@@ -1,5 +1,6 @@
 import { useContext } from 'react';
 import SingleProjectContext from '../../context/SingleProjectContext';
+import ClickableImage from "./ClickableImage";
 
 const ProjectGallery = () => {
 	const { singleProjectData } = useContext(SingleProjectContext);
@@ -8,14 +9,14 @@ const ProjectGallery = () => {
 		<div className="grid grid-cols-1 overflow-hidden sm:grid-cols-3 sm:gap-10 mt-12 ">
 			{singleProjectData.ProjectImages.map((project) => {
 				return (
-					<div className="hover-zoom">
-						<img
-							key={project.id}
-							src={project.img}
-							className="max-h-screen max-w-screen-md object-fill rounded-xl shadow-lg sm:shadow-none hover:scale-125"
-							alt={project.title}
-						/>
-					</div>
+					// <div className="hover-zoom" key={project.id}>
+					// 	<img
+					// 		src={project.img}
+					// 		className="max-h-screen max-w-screen-md object-fill rounded-xl shadow-lg sm:shadow-none hover:scale-125"
+					// 		alt={project.title}
+					// 	/>
+					// </div>
+					<ClickableImage key={project.id} id={project.id} src={project.img} alt={project.title} />
 				);
 			})}
 		</div>
