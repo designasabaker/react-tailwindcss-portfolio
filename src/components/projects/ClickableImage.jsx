eef8f57cc9db6829e896b3c95100521a3366d74b
designasabaker <95067357+designasabaker@users.noreply.github.com>
2023-06-21 16:04:26 -0400

    Add hover zoom to images

--- src/components/projects/ClickableImage.jsx
+++ src/components/projects/ClickableImage.jsx
@@ -18,7 +18,8 @@ export const ClickableImage = (props) => {
     console.log('id isOpen:', id, isOpen)
 
     return (
-        <div>
+        <>
+        <div className={"hover-zoom"}>
             <img
                 key={`${id}-small`}
                 // className="hover-zoom max-h-screen max-w-screen-md object-fill rounded-xl shadow-lg sm:shadow-none hover:scale-125 cursor-pointer"
@@ -28,51 +29,52 @@ export const ClickableImage = (props) => {
                     objectFit: "cover",
                     borderRadius: "0.5rem",
                     cursor: "pointer",
-                    transition: "all 0.2s ease-in-out",
+                    transition: "all 0.5s ease-in-out",
                 }}
                 src={src}
                 alt={alt}
                 onClick={handleSmallImageClick}
             />
-            {/*  larger view */}
-            {isOpen && (
-                <motion.div
-                    animate={{
-                        x: 0,
-                        backgroundColor: "rgba(0,0,0,0.75)",
-                        position: "fixed",
-                        transitionEnd: {
-                            boxShadow: "10px 10px 0 rgba(0, 0, 0, 0.2)",
-                        },
-                    }}
-                    key={`${id}-largeBox`}
-                    style={{
-                        position: "fixed",
-                        width: "100vw",
-                        height: "100vh",
-                        top: 0,
-                        left: 0,
-                        zIndex: 100,
-                    }}
-                    // className={"fixed top-0 left-0 z-100 w-full h-full bg-black bg-opacity-75 flex justify-center items-center"}
-                    onClick={handleLargeImageClick}
-                >
-                    {/*<p>Large</p>*/}
-                    <div className={"w-full h-full flex flex-row justify-center items-center"}>
-                        <img
-                            key={`${id}-large`}
-                            className="block mx-auto contain rounded-lg"
-                            src={src}
-                            alt={alt}
-                            style={{
-                                maxHeight: "66vh",
-                                maxWidth: "66vw",
-                            }}
-                        />
-                    </div>
-                </motion.div>
-            )}
         </div>
+        {/*  larger view */}
+        {isOpen && (
+            <motion.div
+                animate={{
+                    x: 0,
+                    backgroundColor: "rgba(0,0,0,0.75)",
+                    position: "fixed",
+                    transitionEnd: {
+                        boxShadow: "10px 10px 0 rgba(0, 0, 0, 0.2)",
+                    },
+                }}
+                key={`${id}-largeBox`}
+                style={{
+                    position: "fixed",
+                    width: "100vw",
+                    height: "100vh",
+                    top: 0,
+                    left: 0,
+                    zIndex: 100,
+                }}
+                // className={"fixed top-0 left-0 z-100 w-full h-full bg-black bg-opacity-75 flex justify-center items-center"}
+                onClick={handleLargeImageClick}
+            >
+                {/*<p>Large</p>*/}
+                <div className={"w-full h-full flex flex-row justify-center items-center"}>
+                    <img
+                        key={`${id}-large`}
+                        className="block mx-auto contain rounded-lg"
+                        src={src}
+                        alt={alt}
+                        style={{
+                            maxHeight: "66vh",
+                            maxWidth: "66vw",
+                        }}
+                    />
+                </div>
+            </motion.div>
+        )}
+        </>
     )
 }
 
